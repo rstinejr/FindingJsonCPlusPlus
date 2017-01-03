d175f19410bfe39376aafb0f01f7462beca15d9d
R.H. Stine <bob@waltonstine.net>
2017-01-02 19:58:15 -0500

    Add tiny wchar_t example.

--- JsonSerialization/JsonSerialization.cpp
+++ JsonSerialization/JsonSerialization.cpp
@@ -4,23 +4,37 @@
 #include <sstream>
 #include <string>
 
-using boost::property_tree::ptree;
 using namespace std;
 
 int main()
 {
-	ptree pt;
+	boost::property_tree::ptree pt;
 
 	pt.put(string("School"),  string("University of Alabama"));
 	pt.put(string("Cheer"),   string("\"Roll, Tide, Roll!\""));
 	pt.put(string("Sport"),   string("Football"));
 	pt.put(string("Ranking"), 1);
 
-	std::ostringstream out;
+	ostringstream out;
 
 	boost::property_tree::write_json(out, pt);
 
 	cout << "JSON:" << endl << out.str().c_str() << endl << endl;
+
+	cout << endl << "Now for the wide-character verson..." << endl << endl;
+	
+	boost::property_tree::wptree wpt;
+
+	wpt.put(wstring(L"Singer"),   wstring(L"Little Richard"));
+	wpt.put(wstring(L"Song"),     wstring(L"Tutti Frutti"));
+	wpt.put(wstring(L"Recorded"), 1955);
+	wpt.put(wstring(L"refrain"),  wstring(L"\"Wop Bop a Loo La, Mop Bam Boom!\""));
+
+	wostringstream wout;
+
+	boost::property_tree::write_json(wout, wpt, false);
+
+	wcout << L"Wide JSON: " << wout.str().c_str() << endl << endl;
 
 	cout << "Done." << endl;
 
